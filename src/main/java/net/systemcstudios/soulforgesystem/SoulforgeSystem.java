e2ae537b3dd3e451b5aefaba5b58648fc25c036b
Jake <nagyerik012345@gmail.com>
2025-01-23 03:41:46 +0100

    Initial commit

---
+++ src/main/java/net/systemcstudios/soulforgesystem/SoulforgeSystem.java
@@ -0,0 +1,16 @@
+package net.systemcstudios.soulforgesystem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SoulforgeSystem implements ModInitializer {
+	public static final String MOD_ID = "soulforge-system";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/systemcstudios/soulforgesystem/SoulforgeSystemClient.java
@@ -0,0 +1,10 @@
+package net.systemcstudios.soulforgesystem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulforgeSystemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
